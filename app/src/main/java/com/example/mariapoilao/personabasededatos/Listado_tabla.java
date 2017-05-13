ebc554c001bfdae26b8b9ac19af86fbdbed43a30
Maria <mariapoilao@Marias-MacBook-Pro.local>
2017-05-12 20:05:53 -0500

    Interfaz registrar realizada

---
+++ app/src/main/java/com/example/mariapoilao/personabasededatos/Listado_tabla.java
@@ -0,0 +1,13 @@
+package com.example.mariapoilao.personabasededatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Listado_tabla extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_tabla);
+    }
+}
